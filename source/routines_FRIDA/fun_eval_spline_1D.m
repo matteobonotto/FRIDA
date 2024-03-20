20b0d072894c07268053cf91af9fc2978d322f08
matteobonotto <matteob.90@hotmail.it>
2024-03-20 19:18:13 +0100

    hard refactoring

--- source/routines_FRIDA/fun_eval_spline_1D.m
+++
@@ -1,3 +0,0 @@
-function ft = fun_eval_spline_1D(coeffs,t)
-
-ft = coeffs(1,:) + coeffs(2,:).*t + coeffs(3,:).*t.^2 + coeffs(4,:).*t.^3;
--- source/routines_FRIDA/vec_linspace.m
+++
@@ -1,6 +0,0 @@
-function OUT = vec_linspace(vec_start,vec_end,nstep)
-
-OUT = zeros(size(vec_end,1),nstep);
-for ii=1:size(vec_end,1)
-    OUT(ii,:) = linspace(vec_start(ii), vec_end(ii), nstep);
-end
